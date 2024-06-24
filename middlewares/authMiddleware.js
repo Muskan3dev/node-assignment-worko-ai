188a9da99df34e24a217492f76528ca9746cfb6b
Muskan3dev <muskannovember3@gmail.com>
2024-06-24 12:52:37 +0530

    Added authentication middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+
+exports.authenticate = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) return res.status(401).send("Access denied. No token provided.");
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(400).send("Invalid token");
+  }
+};
