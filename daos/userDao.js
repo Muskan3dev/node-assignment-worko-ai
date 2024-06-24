33c2e87f2449624085b886eb3929d7712ad7850b
Muskan3dev <muskannovember3@gmail.com>
2024-06-24 11:30:28 +0530

    Added user dao with crud operations

---
+++ daos/userDao.js
@@ -0,0 +1,22 @@
+import { User } from "../models/userModel";
+
+exports.listUsers = async () => {
+  return await User.find({ isDeleted: false });
+};
+
+exports.getUser = async (userId) => {
+  return await User.findById(userId);
+};
+
+exports.createUser = async (userData) => {
+  const user = new User(userData);
+  return await user.save();
+};
+
+exports.updateUser = async (userId, userData) => {
+  return await User.findByIdAndUpdate(userId, userData, { new: true });
+};
+
+exports.deleteUser = async (userId) => {
+  return await User.findByIdAndDelete(userId);
+};
