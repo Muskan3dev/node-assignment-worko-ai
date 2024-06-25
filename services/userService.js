14196ffa51a7a740e2192701ddfde9186579e0a9
Muskan3dev <muskannovember3@gmail.com>
2024-06-25 20:25:09 +0530

    updated listUsers service by adding isdeleted property for optimized query

--- services/userService.js
+++ services/userService.js
@@ -1,7 +1,10 @@
 const userDao = require("../daos/userDao");
+const User = require("../models/userModel");
 
 exports.listUsers = async () => {
-  return await userDao.listUsers();
+  const users = await User.find({ isDeleted: false });
+  console.log(users);
+  return users;
 };
 
 exports.getUser = async (userId) => {
@@ -13,7 +16,21 @@ exports.createUser = async (userData) => {
 };
 
 exports.updateUser = async (userId, userData) => {
-  return await userDao.updateUser(userId.userData);
+  try {
+    // Remove fields that should not be updated directly
+    if (userData.password) {
+      delete userData.password; // Assuming password should not be updated here
+    }
+
+    // Log the incoming data to debug
+    console.log("Updating user with data:", userData);
+
+    const user = await User.findByIdAndUpdate(userId, userData, { new: true });
+    return user;
+  } catch (error) {
+    console.error("Error updating user:", error);
+    throw error;
+  }
 };
 
 exports.deleteUser = async (userId) => {
