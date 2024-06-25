00cd0ccac482af377995470ddc741cfa6afe1c38
Muskan3dev <muskannovember3@gmail.com>
2024-06-25 20:23:53 +0530

    Added isDeleted and password field

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,13 @@
-import { Schema } from "mongoose";
+const mongoose = require("mongoose");
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   name: { type: String, required: true },
   age: { type: Number, required: true },
   city: { type: String, required: true },
   zipCode: { type: String, required: true },
+  password: { type: String, required: false },
+  isDeleted: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model("User", userSchema);
